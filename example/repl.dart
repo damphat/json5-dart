e169494081a04cc5908c94795fdf306a693ccb27
Phat <damphat@gmail.com>
2020-10-18 05:15:08 +0700

    examples

---
+++ example/repl.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:json5/json5.dart';
+
+void main() {
+  dynamic state;
+  while (true) {
+    stdout.write('json5>');
+    var src = stdin.readLineSync();
+    switch (src) {
+      case '':
+        continue;
+      case '?':
+      case 'h':
+      case 'help':
+      case '.help':
+        print('JSON5 demonstration:');
+        print(' type in your json string');
+        print(' it will print out pretty ');
+        print(' ');
+        print('Commands');
+        print('> help     : document');
+        print('> exit     : quit the app');
+        print('> clear    : clear the console');
+        continue;
+      case 'exit':
+      case 'quit':
+        return;
+      case 'cls':
+      case 'clear':
+        for (var i = 0; i < stdout.terminalLines; i++) {
+          stdout.writeln();
+        }
+        continue;
+    }
+    try {
+      state = JSON5.parse(src);
+      print(JSON5.stringify(state, space: 2));
+    } catch (e) {
+      print(e);
+    }
+  }
+}
